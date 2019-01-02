233041580e1b41cb0f004a5e750b2d096f1b425e
Jeroen Willemsen <jwillemsen@xebia.com>
2019-01-02 22:53:14 +0100

    #135 test 2

--- tools/gendocs.sh
+++ tools/gendocs.sh
@@ -3,6 +3,7 @@ cd $TRAVIS_BUILD_DIR/Tools
 rm ../generated/*.*
 sh ./gitbookepubandpdf.sh $TRAVIS_TAG
 sh ./generate_document.sh $TRAVIS_TAG
+sh ./generate_document_de.sh $TRAVIS_TAG
 sh ./generate_document_es.sh $TRAVIS_TAG
 sh ./generate_document_fr.sh $TRAVIS_TAG
 sh ./generate_document_ja.sh $TRAVIS_TAG
