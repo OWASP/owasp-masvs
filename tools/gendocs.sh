1eaf0eb90577544c9657616be9b8578df5a6e48f
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2019-05-31 21:55:23 +0200

    #211: first step in adding better doc generation support
    Former-commit-id: 4e7f98728164c17a7fadbc709be131042d7d6d02

--- tools/gendocs.sh
+++ tools/gendocs.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/Tools
+echo "Applying Linter check"
+sh ./Apply_Linter_Check.sh
+echo "Counting amount of linter issues:"
+LINTRESULT=$(wc -l ../linter-result.out)
+echo $LINTRESULT
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+    echo "Applying Link check"
+    export LINKRESULT=$(sh ./Apply_Link_Check.sh)
+    echo "$LINKRESULT"
+    echo "Sending feedback to Github"
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"Results for commit $TRAVIS_COMMIT: Broken link result: $LINKRESULT, markdown result errorlength: $LINTRESULT. If fields are empty or 0: you should be OK. If fields contain data: please check for markdown and link errors. \"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
+
+if [ -z "$TRAVIS_TAG" ]; then 
+exit 0; 
+fi
+
+echo "Running creaton of pdfs and word documents"
 rm ../generated/*.*
 sh ./gitbookepubandpdf.sh $TRAVIS_TAG
 sh ./generate_document.sh $TRAVIS_TAG
@@ -9,3 +30,6 @@ sh ./generate_document_fr.sh $TRAVIS_TAG
 sh ./generate_document_ja.sh $TRAVIS_TAG
 sh ./generate_document_ru.sh $TRAVIS_TAG
 sh ./generate_document_zhtw.sh $TRAVIS_TAG
+
+echo "Checking epub validity"
+sh epubcheck ../Generated/OWASP_Mobile_AppSec_Verification_Standard_1.1.3_Document-de.epub
