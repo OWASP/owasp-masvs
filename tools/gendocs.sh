e5d49e04539cc43aa639b75b73004ee1d329f05a
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2019-06-03 09:18:06 +0200

    #211: retest

--- tools/gendocs.sh
+++ tools/gendocs.sh
@@ -3,6 +3,7 @@ cd $TRAVIS_BUILD_DIR/Tools
 #TODO: FIX GITHUB_TOKEN!
 echo "Applying Linter check"
 sh ./Apply_Linter_Check.sh
+echo "Gtihub token: ${GITHUB_TOKEN}"
 echo "Counting amount of linter issues:"
 LINTRESULT=$(wc -l lint-check-result-all-lang.out)
 echo $LINTRESULT
