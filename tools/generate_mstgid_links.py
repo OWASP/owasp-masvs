bc86e4a88a49a6f52ebcd8bc5603dd69e8138314
cpholguera <perezholguera@gmail.com>
2019-12-18 12:21:04 +0100

    added generation of MSTGID github links
    Former-commit-id: c187df1079f408986158823a69591606790bcafa

---
+++ tools/generate_mstgid_links.py
@@ -0,0 +1,110 @@
+# coding: utf-8
+
+""" 
+This script must be run using Python 3.7+ and from its containing folder, i.e. "python3 generate_mstgid_links.py"
+It required the MSTG (owasp-mstg) to be in a folder next to the MASVS (owasp-masvs).
+
+1. generates a MASVS dict containing GitHub links to MSTG markdown sections containing MSTG IDs.
+2. optionally checks the availability of those links.
+3. parses the MSTG-IDs from the current MASVS and calculates coverage: prints all MSTG-IDs not yet covered in the MSTG.
+
+Example:
+
+## MASVS Dict ##
+{
+    "MSTG-NETWORK-3": [
+        "https://github.com/OWASP/owasp-mstg/blob/master/Document/0x05g-Testing-Network-Communication.md#testing-endpoint-identify-verification-mstg-network-3",
+        "https://github.com/OWASP/owasp-mstg/blob/master/Document/0x06g-Testing-Network-Communication.md#testing-custom-certificate-stores-and-certificate-pinning-mstg-network-3-and-mstg-network-4"
+    ],
+    "MSTG-NETWORK-4": [
+        ...
+## COVERAGE ##
+MSTG-RESILIENCE-4 not covered
+MSTG-RESILIENCE-5 not covered
+MSTG-RESILIENCE-6 not covered
+...
+
+"""
+
+import subprocess
+import re
+import json
+from typing import Dict, List
+
+import requests
+
+MSTG_PATH = "../../owasp-mstg/Document"
+MASVS_PATH = "../Document"
+
+DO_URL_LIVE_CHECK = False # If set to True, it will take considerable time
+
+def get_masvs_dict() -> Dict[str, List[str]]:
+
+    parsed = subprocess.run(["egrep", "-r", r"^### (.*\(MSTG-.*-.*\)$)", MSTG_PATH], capture_output=True)
+
+    masvs_dict = {}
+
+    for line in parsed.stdout.split(b"\n"):
+        line = str(line)
+        regex = re.compile(r".*/(.*\.md):### (.*) \((MSTG-.*-.*)\)")
+        match = re.search(regex, line)
+
+        if match:
+            file_name = match.group(1)
+            anchor = match.group(2).replace(" ", "-").lower()
+            mstg_ids_list = [element for element in match.group(3).split() if element.startswith("MSTG")]
+            mstg_ids = match.group(3).replace(" ", "-").lower()
+            url = f"https://github.com/OWASP/owasp-mstg/blob/master/Document/{file_name}#{anchor}-{mstg_ids}"
+
+            for x in mstg_ids_list:
+                if x.endswith(','):
+                    x = x[:-1]
+                if masvs_dict.get(x) is None:
+                    masvs_dict[x] = []
+                masvs_dict[x].append(url) 
+
+                if DO_URL_LIVE_CHECK:
+                    beta_live_check(url)
+
+    return masvs_dict
+
+def beta_live_check(url):
+
+    ret = requests.get(url)
+    if ret.status_code == 200:
+        print("Exists")
+    else:
+        print("Failed")
+
+def get_mstg_ids_from_masvs() -> List[str]:
+
+    mstg_id_regex = r"\*\*\d\.\d+\*\*\s\|\s{0,1}(.*?)\s{0,1}\|"
+
+    parsed_lines = subprocess.run(["egrep", "-r", mstg_id_regex, MASVS_PATH], capture_output=True)
+
+    mstg_ids = []
+
+    for line in parsed_lines.stdout.split(b"\n"):
+        # The MSTG-IDs in the MASVS does not contain regular "-" signs, instead they contain a special character coded as "\xe2\x80\x91"
+        line = str(line.replace(b"\xe2\x80\x91", b"-"))
+        regex = re.compile(mstg_id_regex)
+        match = re.search(regex, line)
+
+        if match:
+            mstg_ids.append(match.group(1))
+    return mstg_ids
+
+def masvs_coverage(masvs_dict, mstg_ids):
+
+    for mstg_id in mstg_ids:
+        if mstg_id not in masvs_dict:
+            print(f"{mstg_id} not covered")
+
+if __name__ == "__main__":
+    print("## MASVS Dict ##\n")
+    masvs_dict = get_masvs_dict()
+    print(json.dumps(masvs_dict, indent=4))
+
+    print("\n\n## COVERAGE ##\n")
+    mstg_ids = get_mstg_ids_from_masvs()
+    masvs_coverage(masvs_dict, mstg_ids)
