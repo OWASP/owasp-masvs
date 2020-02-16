3b12265f171a534b17a145869e05c1531b25bc4e
cpholguera <perezholguera@gmail.com>
2020-02-16 23:16:07 +0100

    added CJK lang for japanese
    Former-commit-id: d224a85ee6771065ee9c5073fc4a5077175ab9ca

--- tools/docker/pandoc_makedocs.sh
+++ tools/docker/pandoc_makedocs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 FOLDER=$1
 VERSION=$2
@@ -12,11 +12,19 @@ CHAPTERS="${FOLDER}/0x*.md ${FOLDER}/CHANGELOG.md"
 sed -e "s/{{MASVS-VERSION}}/$VERSION/g" first_page.tex > tmp_first_page.tex
 sed -e "s/{{MASVS-VERSION}}/$VERSION/g" -e "s/{{MASVS-LANGUAGE}}/$LANGUAGETEXT/g" cover.tex > tmp_cover.tex
 
+if [ $LANGUAGE == "ja" ] || [ $LANGUAGE == "ko" ]
+  then
+  sed -e "s/%%{{CJK}}//g" latex-header.tex > tmp_latex-header.tex
+  else
+  cp latex-header.tex tmp_latex-header.tex
+fi
+
 pandoc --resource-path=.:${FOLDER} \
     --pdf-engine=xelatex --template=eisvogel \
-    --toc -V toc-title:"${TOC_TITLE}" --toc-depth=1 -H \
-    latex-header.tex -V linkcolor:blue --include-before-body tmp_cover.tex --include-before-body tmp_first_page.tex \
+    --toc -V toc-title:"${TOC_TITLE}" --toc-depth=1 \
+    -H tmp_latex-header.tex -V linkcolor:blue --include-before-body tmp_cover.tex --include-before-body tmp_first_page.tex \
     -o ${OUTPUT_BASE_NAME}-${LANGUAGE}.pdf $CHAPTERS
 
 rm tmp_first_page.tex
 rm tmp_cover.tex
+rm tmp_latex-header.tex
