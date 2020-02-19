366ebe224ba0fc6f8a10175f37f1ee2c9dd5f500
cpholguera <perezholguera@gmail.com>
2020-02-19 20:34:31 +0100

    added language specific tmp files for multithreading; added multithreading for local
    Former-commit-id: f357fe47647bf9130d361edba66546c24aa378d7

--- docker_masvs_gen.sh
+++ docker_masvs_gen.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-#FOLDER=$1 # e.g. Document-es
 VERSION=$1 # e.g. 1.2
 
 export IMG="masvs-generator:latest"
@@ -13,5 +12,7 @@ fi
 
 for folder in ./Document*; do
   echo "Generating $folder"
-  docker run --rm -u `id -u`:`id -g` -v ${PWD}:/pandoc $IMG "/pandoc_makedocs.sh $folder $VERSION" || echo "$folder failed"
+  docker run --rm -u `id -u`:`id -g` -v ${PWD}:/pandoc $IMG "/pandoc_makedocs.sh $folder $VERSION" || echo "$folder failed" &
 done
+
+wait
