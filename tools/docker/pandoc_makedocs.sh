ded31c45ea94d8c91bf9648c95c5da85377cac38
Sven <sven@bsddaemon.org>
2024-06-14 09:52:31 +0200

    Update pandoc_makedocs.sh

--- tools/docker/pandoc_makedocs.sh
+++ tools/docker/pandoc_makedocs.sh
@@ -9,7 +9,7 @@ MASTG_VERSION=${3:-SNAPSHOT}
 
 # You can also use the environment variables below to adapt the build process
 IMG=${IMG:-dalibo/pandocker}
-TAG=${TAG:-stable} # /!\ use stable-full for non-european languages
+TAG=${TAG:-23.03} # /!\ use stable-full for non-european languages
 LATEX_TEMPLATE=${LATEX_TEMPLATE:-eisvogel}
 TITLE=${TITLE:-OWASP Mobile Application Security Verification Standard ${MASVS_VERSION}}
 
