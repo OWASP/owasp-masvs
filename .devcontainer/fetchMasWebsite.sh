bd7831d8399cc48af9cc1f73b3d3da2c978061fa
bernhste <s.bernhardsgruetter@proton.me>
2026-09-12 20:07:45 +0000

    updated fetchMasWebsite to fetch from upstream

--- .devcontainer/fetchMasWebsite.sh
+++ .devcontainer/fetchMasWebsite.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# TODO: REVERT before PR
-repo_url="https://github.com/bernhste/mas-website.git"
-# repo_url="https://github.com/OWASP/mas-website.git"
+repo_url="https://github.com/OWASP/mas-website.git"
 dir="/workspace/mas-website"
 
-# TODO: REMOVE before PR
-branch="new-devcontainer"
-
 if [ -d "${dir}/.git" ]; then
-  # TODO: REVERT before PR
-  # git -C "${dir}" fetch --depth 1 origin
-  # git -C "${dir}" reset --hard origin/HEAD
-  git -C "${dir}" fetch --depth 1 origin "${branch}"
-  git -C "${dir}" checkout -B "${branch}" FETCH_HEAD
-  git -C "${dir}" reset --hard FETCH_HEAD
+
+  git -C "${dir}" fetch --depth 1 origin
+  git -C "${dir}" reset --hard origin/HEAD
 else
-  # git clone --depth 1 "${repo_url}" "${dir}"
-  git clone --depth 1 --branch "${branch}" --single-branch "${repo_url}" "${dir}"
+  git clone --depth 1 "${repo_url}" "${dir}"
 fi
-
-
